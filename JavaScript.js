de7c52516299b0836ac6b720a4ad970d71fba324
KageKamigami <123619857+KageKamigami@users.noreply.github.com>
2024-02-10 14:49:48 -0600

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,5 @@
 
 function login() {
-    // Get the email and password entered by the user
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-
     if (email === 'ale@gmail.com' && password === 'meowsss') {
         window.location.href = "student_view.html";
     } else {
