2a6eb3b0c9916463e1f039cff1b3c10d98fd65b9
KageKamigami <123619857+KageKamigami@users.noreply.github.com>
2024-02-10 15:02:41 -0600

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -6,3 +6,12 @@ function login() {
         alert('Invalid email or password. Please try again.');
     }
 }
+
+function code(string studentCode){
+    if(studentCode == "hack123"){
+        window.location.href = "balance_view.html";
+    }
+    else{
+        alert('Invalid code. Please try again.');
+    }
+}
